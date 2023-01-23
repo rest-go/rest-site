208d7fa064dc42d4c59d0ed5201ca645fe36c3bb
shellfly <shell0fly@gmail.com>
2023-01-23 10:17:59 +0800

    feat: gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-VMZ8SCP7GB',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
